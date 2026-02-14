1c4d48145acd59b89a317dfe1d037f5744632aee
FamilyFriendly77 <116194178+FamilyFriendly77@users.noreply.github.com>
2026-02-15 00:49:40 +0100

    moves map

--- pkg/cube/cube.go
+++ pkg/cube/cube.go
@@ -8,7 +8,28 @@ type CubeModel struct {
 	Sides []Side
 }
 
-func (cube *CubeModel) scrambleCube() {
+var moves = map[string]func(int, *CubeModel){
+	"R": func(r int, cube *CubeModel) {},
+	"L": func(r int, cube *CubeModel) {},
+	"U": func(r int, cube *CubeModel) {},
+	"D": func(r int, cube *CubeModel) {},
+	"F": func(r int, cube *CubeModel) {},
+	"B": func(r int, cube *CubeModel) {},
+	"r": func(r int, cube *CubeModel) {},
+	"l": func(r int, cube *CubeModel) {},
+	"u": func(r int, cube *CubeModel) {},
+	"d": func(r int, cube *CubeModel) {},
+	"f": func(r int, cube *CubeModel) {},
+	"b": func(r int, cube *CubeModel) {},
+	"x": func(r int, cube *CubeModel) {},
+	"y": func(r int, cube *CubeModel) {},
+	"z": func(r int, cube *CubeModel) {},
+	"M": func(r int, cube *CubeModel) {},
+	"E": func(r int, cube *CubeModel) {},
+	"S": func(r int, cube *CubeModel) {},
+}
+
+func (cube *CubeModel) ScrambleCube(scramble string) {
 
 }
 func (cube *CubeModel) printCube() {
